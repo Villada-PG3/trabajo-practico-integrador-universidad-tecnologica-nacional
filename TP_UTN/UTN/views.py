b0fe7b785896e241c03416c1298bb2845f52c194
Ojodedragon <luchosibona@gmail.com>
2025-08-25 11:57:19 -0300

    todas las views

--- TP_UTN/UTN/views.py
+++ TP_UTN/UTN/views.py
@@ -6,52 +6,77 @@ from .models import Alumno, Curso, Materia, MateriaCurso, AlumnoCurso, Inscripci
 # Views for Alumno
 class AlumnoListView(ListView):
     model = Alumno
-    template_name = 'alumno_list.html'
+    template_name = 'alumno/alumno_list.html'
 
 class AlumnoDetailView(DetailView):
     model = Alumno
-    template_name = 'alumno_detail.html'
+    template_name = 'alumno/alumno_detail.html'
 
 class AlumnoCreateView(CreateView):
     model = Alumno
     fields = '__all__'
-    template_name = 'alumno_form.html'
+    template_name = 'alumno/alumno_form.html'
     success_url = reverse_lazy('alumno_list')
 
 class AlumnoUpdateView(UpdateView):
     model = Alumno
     fields = '__all__'
-    template_name = 'alumno_form.html'
+    template_name = 'alumno/alumno_form.html'
     success_url = reverse_lazy('alumno_list')
 
 class AlumnoDeleteView(DeleteView):
     model = Alumno
-    template_name = 'alumno_confirm_delete.html'
+    template_name = 'alumno/alumno_confirm_delete.html'
     success_url = reverse_lazy('alumno_list')
 
+# Views for Curso
 class CursoListView(ListView):
     model = Curso
-    template_name = 'curso_list.html'
+    template_name = 'curso/curso_list.html'
 
 class CursoDetailView(DetailView):
     model = Curso
-    template_name = 'curso_detail.html'
+    template_name = 'curso/curso_detail.html'
 
 class CursoCreateView(CreateView):
     model = Curso
     fields = '__all__'
-    template_name = 'curso_form.html'
+    template_name = 'curso/curso_form.html'
     success_url = reverse_lazy('curso_list')
 
 class CursoUpdateView(UpdateView):
     model = Curso
     fields = '__all__'
-    template_name = 'curso_form.html'
+    template_name = 'curso/curso_form.html'
     success_url = reverse_lazy('curso_list')
 
 class CursoDeleteView(DeleteView):
     model = Curso
-    template_name = 'curso_confirm_delete.html'
+    template_name = 'curso/curso_confirm_delete.html'
     success_url = reverse_lazy('curso_list')
 
-# Repeat for Materia, MateriaCurso, AlumnoCurso, Inscripcion, TipoEvaluacion, Reporte, CondicionFinal, Evaluacion
+# Views for Materia
+class MateriaListView(ListView):
+    model = Materia
+    template_name = 'materia/materia_list.html'
+
+class MateriaDetailView(DetailView):
+    model = Materia
+    template_name = 'materia/materia_detail.html'
+
+class MateriaCreateView(CreateView):
+    model = Materia
+    fields = '__all__'
+    template_name = 'materia/materia_form.html'
+    success_url = reverse_lazy('materia_list')
+
+class MateriaUpdateView(UpdateView):
+    model = Materia
+    fields = '__all__'
+    template_name = 'materia/materia_form.html'
+    success_url = reverse_lazy('materia_list')
+
+class MateriaDeleteView(DeleteView):
+    model = Materia
+    template_name = 'materia/materia_confirm_delete.html'
+    success_url = reverse_lazy('materia_list')
