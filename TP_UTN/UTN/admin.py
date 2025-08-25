265e12ecea563e39d39ac113c1a017fa098f2b29
Ojodedragon <luchosibona@gmail.com>
2025-08-25 11:43:58 -0300

    admin site working

--- TP_UTN/UTN/admin.py
+++ TP_UTN/UTN/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import MateriaCurso, Alumno, Curso, Materia, AlumnoCurso, Inscripcion, TipoEvaluacion, Reporte, CondicionFinal, Evaluacion
 
 # Register your models here.
+admin.site.register(MateriaCurso)
+admin.site.register(Alumno)
+admin.site.register(Curso)
+admin.site.register(Materia)
+admin.site.register(AlumnoCurso)
+admin.site.register(Inscripcion)
+admin.site.register(TipoEvaluacion)
+admin.site.register(Reporte)
+admin.site.register(CondicionFinal)
+admin.site.register(Evaluacion)
